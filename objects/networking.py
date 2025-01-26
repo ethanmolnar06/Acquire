71fe62758ecf5dafd5c3dd087233464611b64927
ethanmolnar06 <157933281+ethanmolnar06@users.noreply.github.com>
2025-01-25 21:37:22 -0500

    IT REALLY WORKS! TCP fully operational and significantly faster to load up design ansi color tag eraser so many bug fixes it's unreal

--- objects/networking.py
+++ objects/networking.py
@@ -3,6 +3,8 @@ import threading
 import uuid
 import subprocess
 import shutil
+import re
+from io import BytesIO
 from typing import Callable
 from requests import get
 
@@ -10,59 +12,105 @@ from objects.connection import Command, KillableThread, Connection, PORT, DISCON
 from objects.player import Player
 
 class Proxy():
+  prox: dict[str, dict[str, str | re.Pattern]] = {
+      "serveo.net": { # https://groups.google.com/g/serveo & https://github.com/u1i/tools/blob/master/serveo.md
+        "subdom": "",
+        "port": f"{PORT}",
+        "success": re.compile(r"Forwarding (?P<p>\w+) \w+ from (?:\w+?:\/\/)?(?P<u>\w+?.serveo.net|[\w.]+?:\d+)"),
+      }, 
+      # "localhost.run": { # https://localhost.run/docs/ 
+      #   "subdom": "",
+      #   "port": f"80",
+      #   "success": re.compile(r"\w+?\.lhr\.life tunneled with tls termination, (?P<p>\w+?):\/\/(?P<u>\w+?.\.lhr\.life)"),
+      # },
+    }
+  
   def __init__(self):
     self.tunnelBuilt = False
-    self.proxy_url = None
+    self.protocol = None
+    self.hostname = None
+    self.addr = None
+    self.port = None
     
-    self.proxies = {
-      "serveo.net": {b"Forwarding HTTP traffic from",}, # https://groups.google.com/g/serveo & https://github.com/u1i/tools/blob/master/serveo.md
-      "localhost.run": {b"tunneled with tls termination",}, # https://localhost.run/docs/
-    }
-    
-    for prox in self.proxies.keys():
-      ssh_cmd = f"ssh -o ServerAliveInterval=60 -R 80:127.0.0.1:{PORT} nokey@{prox}".split()
+    for px in self.prox.keys():
+      self.port = self.prox[px]["port"]
+      ssh_cmd = f"ssh -o ServerAliveInterval=60 -R {self.prox[px]["subdom"] + self.port}:127.0.0.1:{PORT} nokey@{px}".split()
       
-      def clean_response(stdout: bytes) -> str | None:
-        if stdout and any([fil in stdout.strip() for fil in self.proxies[prox]]):
-          # expecting https://XXXXXXXXXXXXXX.{proxy}.XXXX
-          return "https://" + stdout.strip().decode().split("https://")[-1]
+      def validate(stdout: bytes) -> tuple[str] | None:
+        self._proxy_print("stdout ==", stdout.strip().decode())
+        match = self.prox[px]["success"].search(stdout.strip().decode())
+        self._proxy_print("matchObj ==", match)
+        if match is None:
+          return
+        protocol_url = (match.group("p").upper(), match.group("u"))
+        self._proxy_print("groups ==", protocol_url)
+        return protocol_url
       
       try:
-        test_stdout = subprocess.run(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, timeout=1.5).stdout
+        testrun = del_ansi(subprocess.run(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, timeout=2).stdout)
       except subprocess.TimeoutExpired as e:
-        test_stdout = e.stdout
+        testrun = del_ansi(e.stdout)
+      cleaned_std = [l for l in BytesIO(testrun).readlines() if l]
+      lastline = len(cleaned_std)
+      self._proxy_print(cleaned_std, lastline)
       
-      if clean_response(test_stdout) is not None:
+      if validate(testrun):
         print("[PROXY] Opening Reverse Proxy for Public Clients")
         break
     
-    self.process = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
-    self.thread = KillableThread(target=self.reverse_proxy, args=(clean_response,))
+    self.process = subprocess.Popen(ssh_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL, process_group=0)
+    self.thread = KillableThread(target=self.reverse_proxy, args=(validate, lastline))
     self.thread.start()
+  
+  def _proxy_print(self, *var):
+    from common import PRINT_PROXY_DEBUG
+    if PRINT_PROXY_DEBUG:
+      print(*var)
   
   def kill(self):
     self.process.kill()
     self.thread.kill()
   
-  def reverse_proxy(self, kill_event: threading.Event, clean_response: Callable[[bytes], str | None]):
-    while not kill_event.isSet() and self.process.poll() is None:
-      response = self.process.stdout.readline().strip()
-      if not response:
+  def fetch_ip(self):
+    if self.protocol == "TCP":
+      self.addr = socket.gethostbyname(self.hostname)
+    elif self.protocol == "HTTPS":
+      self.addr = socket.gethostbyname(self.hostname)
+    self._proxy_print(" [[", self.protocol, self.hostname, self.addr, self.port, "]] ")
+  
+  def reverse_proxy(self, kill_event: threading.Event, validate: Callable[[str], tuple[str] | None], lastline: int):
+    i = 1
+    while self.process.poll() is None:
+      if kill_event.is_set():
+        self.process.kill()
+      cur_line = del_ansi(self.process.stdout.readline())
+      response = validate(cur_line)
+      self._proxy_print(response)
+      if i == lastline and not self.tunnelBuilt and response is None:
+        break
+      elif response is None:
+        i += 1 if cur_line else 0
         continue
-      url = clean_response(response)
-      if url is None:
-        continue
-      self.proxy_url = url
-      if not self.tunnelBuilt:
-        print("[PROXY SUCCESS] Reverse Proxy Open for Public Clients")
-        self.tunnelBuilt = True
-      else:
-        print("[PROXY REFRESHED] New Reverse Proxy Link")
+      
+      self.protocol = response[0]
+      self.hostname = response[1].removesuffix(f":{self.port}")
+      try:
+        self.fetch_ip()
+        if not self.tunnelBuilt:
+          print("[PROXY SUCCESS] Reverse Proxy Established")
+          self.tunnelBuilt = True
+          continue
+        else:
+          print(f"[PROXY REFRESHED] New Reverse Proxy Link {self.addr}")
+      except socket.error as e:
+        self.hostname = None
+        self.kill()
+        raise e
     
-    if self.proxy_url is None:
-      print("[PROXY FAILURE] Reverse Proxy Failed to Connect")
+    if self.hostname is None:
+      print("[PROXY FAILURE] Reverse Proxy Not Online, Try Port Forwarding")
     else:
-      print("[PROXY CLOSED] Reverse Proxy Disconnected")
+      print("[PROXY CLOSED] Reverse Proxy Link Deactivated")
     
     self.process.kill()
 
@@ -95,8 +143,8 @@ def start_server(conn_dict:dict[uuid.UUID, Connection], newGame: bool, gameState
     if ALLOW_REVERSE_PROXY and shutil.which("ssh"):
       reverseProxy = Proxy()
       while reverseProxy.thread.is_alive():
-        if reverseProxy.tunnelBuilt and reverseProxy.proxy_url is not None:
-          listening_str += f" & {reverseProxy.proxy_url} (public)"
+        if reverseProxy.tunnelBuilt and reverseProxy.hostname is not None:
+          listening_str += f" & {reverseProxy.hostname} (public)"
           break
     # endregion
     
@@ -111,7 +159,7 @@ def start_server(conn_dict:dict[uuid.UUID, Connection], newGame: bool, gameState
   from common import pack_gameState
   def accept_conn(kill_event:threading.Event, newGame: bool, gameState: tuple):
     server.settimeout(.5) # time to wait in sec
-    while not kill_event.isSet():
+    while not kill_event.is_set():
       try:
         client, addr = server.accept()
       except TimeoutError:
@@ -135,7 +183,7 @@ def start_server(conn_dict:dict[uuid.UUID, Connection], newGame: bool, gameState
   
   return serverThread, reverseProxy, conn_dict
 
-def start_client(ip, conn_dict) -> dict[str, Connection]:
+def start_client(ip, conn_dict: dict[str, Connection]) -> dict[str, Connection]:
   client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
   client.connect((ip, PORT))
   
@@ -154,7 +202,8 @@ def extrctConns(collection: dict[uuid.UUID, Connection] | list[Player] | list[Co
   else:
     raise TypeError("Invalid Source of Connections")
 
-def propagate(dests: dict[uuid.UUID, Connection] | list[Player] | list[Connection], source: uuid.UUID | Player | Connection | None, command: Command):
+def propagate(dests: dict[uuid.UUID, Connection] | list[Player] | list[Connection],
+              source: uuid.UUID | Player | Connection | None, command: Command):
   conns = extrctConns(dests)
   for conn in conns:
     # should be safe to just drop in for hostLocal
@@ -180,3 +229,7 @@ def fetch_updates(sources: dict[uuid.UUID, Connection] | list[Player] | list[Con
       u = (conn.uuid, conn.fetch())
       updates.append(u)
   return updates
+
+def del_ansi(line: bytes) -> bytes:
+  ansi_pat = re.compile(rb"(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]")
+  return ansi_pat.sub(b"", line)
